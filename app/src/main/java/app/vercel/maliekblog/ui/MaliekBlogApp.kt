e4c8539401f4149c1ebb21bb50eddc4ba9908a9c
MalickGalant <89748688+MalickGalant@users.noreply.github.com>
2022-12-18 14:29:24 +0200

    Create MaliekBlogApp.kt

---
+++ app/src/main/java/app/vercel/maliekblog/ui/MaliekBlogApp.kt
@@ -0,0 +1,78 @@
+/*
+ * Copyright 2020 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package app.vercel.maliekblog.ui
+
+import androidx.compose.material.AlertDialog
+import androidx.compose.material.Text
+import androidx.compose.material.TextButton
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.res.stringResource
+import androidx.lifecycle.viewmodel.compose.viewModel
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import app.vercel.maliekblog.R
+import app.vercel.maliekblog.ui.home.Home
+import app.vercel.maliekblog.ui.player.PlayerScreen
+import app.vercel.maliekblog.ui.player.PlayerViewModel
+import app.vercel.maliekblog.util.DevicePosture
+import kotlinx.coroutines.flow.StateFlow
+
+@Composable
+fun MaliekBlogApp(
+    devicePosture: StateFlow<DevicePosture>,
+    appState: MaliekBlogAppState = rememberMaliekBlogAppState()
+) {
+    if (appState.isOnline) {
+        NavHost(
+            navController = appState.navController,
+            startDestination = Screen.Home.route
+        ) {
+            composable(Screen.Home.route) { backStackEntry ->
+                Home(
+                    navigateToPlayer = { episodeUri ->
+                        appState.navigateToPlayer(episodeUri, backStackEntry)
+                    }
+                )
+            }
+            composable(Screen.Player.route) { backStackEntry ->
+                val playerViewModel: PlayerViewModel = viewModel(
+                    factory = PlayerViewModel.provideFactory(
+                        owner = backStackEntry,
+                        defaultArgs = backStackEntry.arguments
+                    )
+                )
+                PlayerScreen(playerViewModel, devicePosture, onBackPress = appState::navigateBack)
+            }
+        }
+    } else {
+        OfflineDialog { appState.refreshOnline() }
+    }
+}
+
+@Composable
+fun OfflineDialog(onRetry: () -> Unit) {
+    AlertDialog(
+        onDismissRequest = {},
+        title = { Text(text = stringResource(R.string.connection_error_title)) },
+        text = { Text(text = stringResource(R.string.connection_error_message)) },
+        confirmButton = {
+            TextButton(onClick = onRetry) {
+                Text(stringResource(R.string.retry_label))
+            }
+        }
+    )
+}
